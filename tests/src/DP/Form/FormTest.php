3171c9de694e84373b0d65e4b7e99a87d24ebe1f
Flavio Silva <flv.alves@gmail.com>
2015-05-24 14:30:01 -0300

    criando test para funcionalidade de popular dados do formulário

--- tests/src/DP/Form/FormTest.php
+++ tests/src/DP/Form/FormTest.php
@@ -2,8 +2,68 @@
 
 namespace DP\Form;
 
+use DP\Validator\Validator;
+use DP\Request\Request;
+use DP\Form\Input;
+use DP\Form\Select;
+
 class FormTest extends \PHPUnit_Framework_TestCase
 {
+    private $form;
+    private $data;
+    private $categorias;
+     
+    public function setUp()
+    {   
+        #Configuração das variáveis super globais
+        $_SERVER['REQUEST_METHOD'] = 'POST';        
+        $_POST = [
+            'nome' => 'Notebook',
+            'valor' => '2000',
+            'descricao' => 'Dell Inspiron',
+            'categoria' => '2'
+        ];
+        
+        $request = new Request();
+        $post = $request->getPost();
+        $this->data = $post->getData();
+        $this->form = new Form(new Validator($request), 'form-test');
+        
+        $this->categorias = [
+            1 => 'Eletrodoméstico',
+            2 => 'Eletrônico',
+            3 => 'Cama, Mesa e Banho',
+            4 => 'Brinquedos',
+        ];
+    }
+    
+    public function testVerificaAFuncionalidadeDePopularDados()
+    {               
+        $nome = new Input('nome');
+        $nome->setAttribute('type', 'text');
+
+        $valor = new Input('valor');
+        $valor->setAttribute('type', 'text');
+
+        $descricao = new Input('descricao');
+        $descricao->setAttribute('type', 'text');
+
+        $categoria = new Select('categoria');
+        
+        $this->form->createField($nome)
+            ->createField($valor)
+            ->createField($descricao)
+            ->createField($categoria->setValueOptions($this->categorias))
+        ;
+        
+        $this->form->populate($this->data);
+        
+        $this->assertEquals('Notebook', $this->form->getField('nome')->getAttribute('value'));
+        $this->assertEquals('2000', $this->form->getField('valor')->getAttribute('value'));
+        $this->assertEquals('Dell Inspiron', $this->form->getField('descricao')->getAttribute('value'));
+        $this->assertEquals('2', $this->form->getField('categoria')->getSelected());
+    }
+    
     public function getForm()
     {
         $validator = $this->getMockBuilder('\DP\Validator\Validator')
